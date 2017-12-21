d3ee9e074401a4d330b319d0e6f6432cd88b0e14
christoph Niemann <christoph.niemann@netways.de>
2017-12-21 19:54:38 +0100

    find

--- tests/test.sh
+++ tests/test.sh
@@ -99,6 +99,7 @@ if [ "$test_idempotence" = true ]; then
     || (printf ${red}'Idempotence test: fail'${neutral}"\n" && exit 1)
 fi
 
+docker exec $container_id env TERM=xterm echo "FIND SCHEMA"
 docker exec $container_id env TERM=xterm find / -name "mysql.schema.sql"
 
 # Remove the Docker container (if configured).
